164e510ed17cd1699bf7c223802ed4e6c2cf2720
Jaspalsinh Chauhan <jachauhan@gmail.com>
2015-06-04 02:12:08 +0530

    Some cooking on string case converter, reading more on casing wiki

---
+++ source/booger-wpf-dotnet/Converters/StringCaseConverter.cs
@@ -0,0 +1,61 @@
+﻿#region Copyright Jsinh Booger WPF .NET
+// ************************************************************************************
+// <copyright file="StringCaseConverter.cs" company="Jaspalsinh Chauhan">
+// Copyright © Jaspalsinh Chauhan 2015. All right reserved.
+// </copyright>
+// ************************************************************************************
+// <author>Jaspalsinh Chauhan</author>
+// <url>http://jsinh.in</url>
+// <url>https://github.com/jsinh/booger-wpf-net</url>
+// ************************************************************************************
+#endregion
+
+// ReSharper disable CheckNamespace
+namespace Jsinh.BoogerWpf
+// ReSharper restore CheckNamespace
+{
+    #region Namespace
+
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    
+    #endregion
+
+    /// <summary>
+    /// Represents booger for conversion between different case types for string values.
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(string))]
+    public sealed class StringCaseConverter : IValueConverter
+    {
+        #region Methods
+
+        /// <summary>
+        /// Convert from source to destination desired value.
+        /// </summary>
+        /// <param name="value">Input source value.</param>
+        /// <param name="targetType">Instance of target type.</param>
+        /// <param name="parameter">Convert parameter to use.</param>
+        /// <param name="culture">Culture to be used during conversion</param>
+        /// <returns>Returns case changed value.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Convert destination value back to source expected value.
+        /// </summary>
+        /// <param name="value">Input destination value.</param>
+        /// <param name="targetType">Instance of target type.</param>
+        /// <param name="parameter">Convert parameter to use.</param>
+        /// <param name="culture">Culture to be used during conversion</param>
+        /// <returns>Returns case changed value..</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Empty;
+        }
+
+        #endregion
+    }
+}
---
+++ source/booger-wpf-dotnet/Enums/CaseStyle.cs
@@ -0,0 +1,47 @@
+﻿#region Copyright Jsinh Booger WPF .NET
+// ************************************************************************************
+// <copyright file="CaseStyle.cs" company="Jaspalsinh Chauhan">
+// Copyright © Jaspalsinh Chauhan 2015. All right reserved.
+// </copyright>
+// ************************************************************************************
+// <author>Jaspalsinh Chauhan</author>
+// <url>http://jsinh.in</url>
+// <url>https://github.com/jsinh/booger-wpf-net</url>
+// ************************************************************************************
+#endregion
+
+// ReSharper disable CheckNamespace
+namespace Jsinh.BoogerWpf
+// ReSharper restore CheckNamespace
+{
+    /// <summary>
+    /// Defines a list of all case styles that <seealso cref="StringCaseConverter"/> can support.
+    /// </summary>
+    public enum CaseStyle
+    {
+        /// <summary>
+        /// All words in input string will be converted to capital case.
+        /// </summary>
+        AllCaps,
+
+        /// <summary>
+        /// All words in input string will be converted to lower case.
+        /// </summary>
+        AllLower,
+
+        /// <summary>
+        /// First letter of the first word within a sentence will be converted to lower case.
+        /// </summary>
+        CamelCase,
+
+        /// <summary>
+        /// First letter of the first word within a sentence will be converted to upper case.
+        /// </summary>
+        SentenceCase,
+
+        /// <summary>
+        /// First letter of all words found in string will be converted to upper case.
+        /// </summary>
+        TitleCase
+    }
+}
